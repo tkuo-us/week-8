89a230fca42a03dd6ffa2f066655e0e3b07b6d3a
leon todd johnson <126616861+leontoddjohnson@users.noreply.github.com>
2025-10-01 23:22:45 +0000

    update exercises

--- apputil.py
+++ apputil.py
@@ -1,1 +1,23 @@
-# your code here
+from collections import defaultdict
+
+
+class MarkovText(object):
+
+    def __init__(self, corpus):
+        self.corpus = corpus
+        self.quote_dict = None  # you'll need to build this
+
+    def get_quote_dict(self):
+
+        # your code here ...
+
+        self.quote_dict = {}
+
+        return None
+
+
+    def generate(self, term_count=15, seed_term=None):
+
+        # your code here ...
+
+        return None
